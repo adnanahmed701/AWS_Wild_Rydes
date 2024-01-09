35d8bddbbbe97900a50040e7f5832dc95b87d904
Adnan Ahmed <101381610+adnanahmed701@users.noreply.github.com>
2024-01-09 05:11:48 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_qkPLjr2Ea', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '36me6m8prv3g71b6dicoupc2mf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://vi85j725f9.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
